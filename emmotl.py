5e436a0b224c441f90960fa95d8d723fdce7892e
Eliska <eliskamalanikova@gmail.com>
2022-07-12 09:42:31 +0200

    Update class stump

--- emmotl.py
+++ emmotl.py
@@ -4,21 +4,23 @@ import emfile
 
 
 class Motl:
-    ALLOWED_SOURCES = ['emfile']  # allowed source files
+    # Motl module example usage
+    #
+    # Initialize a Motl instance from an emfile
+    #   `motl = Motl.load(’path_to_em_file’)`
+    # Run clean_by_otsu and write the result to a new file
+    #   `motl.clean_by_otsu(4, histogram_bin=20).write_to_emfile('path_to_output_em_file')`
+    # Run class_consistency on multiple Motl instances
+    #   `motl_intersect, motl_bad,cl_overlap = Motl.class_consistency(Motl.load('emfile1', 'emfile2', 'emfile3'))`
 
-    def __init__(self, motl_df, source):
-        self.motl_df = motl_df
-        if source in self.ALLOWED_SOURCES:
-            self.source = source
-        else:
-            raise
+    def __init__(self, motl_df):
+        self.df = motl_df
 
-    @classmethod
-    def create_empty_motl(cls, number_of_particles):
-        empty_motl_df = pd.DataFrame(data=np.zeros(number_of_particles, 20))
-        empty_motl_df.columns = ['score', 'geom1', 'geom2', 'subtomo_id', 'tomo_id', 'object_id', 'subtomo_mean',
-                                 'x', 'y', 'z', 'shift_x', 'shift_y', 'shift_z', 'geom4', 'geom5', 'geom6',
-                                 'phi', 'psi', 'theta', 'class']
+    @staticmethod
+    def create_empty_motl():
+        empty_motl_df = pd.DataFrame(columns = ['score', 'geom1', 'geom2', 'subtomo_id', 'tomo_id', 'object_id',
+                                                'subtomo_mean', 'x', 'y', 'z', 'shift_x', 'shift_y', 'shift_z', 'geom4',
+                                                'geom5', 'geom6', 'phi', 'psi', 'theta', 'class'])
         return empty_motl_df
 
     @classmethod
@@ -27,7 +29,7 @@ class Motl:
         tomo_number, object_number, coordinates, angles = emfile_path
         number_of_particles = coordinates.shape[0]
 
-        motl = cls.create_empty_motl(number_of_particles)
+        motl = cls.create_empty_motl()
         motl['subtomo_id'] = np.arange(1, number_of_particles + 1, 1)
         motl['tomo_id'] = tomo_number
         motl['object_id'] = object_number
@@ -42,16 +44,12 @@ class Motl:
         # assign angles
         motl[['phi', 'psi', 'theta']] = angles.values
 
-        return cls(motl, 'emfile')
+        return cls(motl)
 
     def write_to_emfile(self, outfile_path):
-        if self.source == 'emfile':
-            motl_array = self.motl_df.values
-            motl_array = motl_array.reshape((1, motl_array.shape[0], motl_array.shape[1]))
-
-            emfile.write(outfile_path, motl_array, overwrite=True)
-        else:
-            raise f'Sorry, the origin of the data is {self.source}, which is not compatible with the EMfile format.'
+        motl_array = self.df.values
+        motl_array = motl_array.reshape((1, motl_array.shape[0], motl_array.shape[1]))
+        emfile.write(outfile_path, motl_array, overwrite=True)
 
     @staticmethod
     def motl_batch_stopgap2em(motl_base_name,iteration_range):
